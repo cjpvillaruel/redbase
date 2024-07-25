98fd0e5d13eb1a93a1c58596fb6895a8e68d6803
Christine Joy Villaruel <cjpvillaruel@gmail.com>
2024-07-26 03:14:29 +0800

    Change button colr

--- app/javascript/components/Button.jsx
+++ app/javascript/components/Button.jsx
@@ -1,18 +1,18 @@
 // create button component  using tailwind with border radius of 2
 import React from "react";
 const Button = ({ children, variant, className }) => {
-  const defaultClasses = "font-bold py-2 px-4 rounded-md flex";
-  let buttonClass = `bg-sky-400 hover:bg-sky-600 text-white`;
+  const defaultClasses = "font-semibold py-2 px-4 rounded-md flex";
+  let buttonClass = `bg-sky-500 hover:bg-sky-600 text-white`;
 
   if (variant === "primary") {
-    buttonClass = `bg-sky-400 hover:bg-sky-600 text-white`;
+    buttonClass = `bg-sky-500 hover:bg-sky-600 text-white`;
   } else if (variant === "secondary") {
     buttonClass =
-      "text-sky-400 bg-transparent hover:bg-sky-400 hover:text-white border border-sky-400 border-width-1";
+      "text-sky-500 bg-transparent hover:bg-sky-500 hover:text-white border border-sky-500 border-width-1";
   } else if (variant === "danger") {
     buttonClass = `bg-red-500 hover:bg-red-700 text-white`;
   } else if (variant === "text") {
-    buttonClass = `text-blue-500 hover:text-sky-400`;
+    buttonClass = `text-blue-500 hover:text-sky-500`;
   }
 
   return (
