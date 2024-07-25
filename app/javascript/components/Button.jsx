40e0c390206554276401f8820e7cd472af92c0f4
Christine Joy Villaruel <cjpvillaruel@gmail.com>
2024-07-26 03:11:43 +0800

    Add icons and make buttons responsive

--- app/javascript/components/Button.jsx
+++ app/javascript/components/Button.jsx
@@ -1,24 +1,25 @@
 // create button component  using tailwind with border radius of 2
 import React from "react";
 const Button = ({ children, variant, className }) => {
-  let buttonClass =
-    "bg-sky-400 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded-md";
+  const defaultClasses = "font-bold py-2 px-4 rounded-md flex";
+  let buttonClass = `bg-sky-400 hover:bg-sky-600 text-white`;
 
   if (variant === "primary") {
-    buttonClass =
-      "bg-sky-400 hover:bg-sky-600 text-white font-bold py-2 px-4 rounded-md";
+    buttonClass = `bg-sky-400 hover:bg-sky-600 text-white`;
   } else if (variant === "secondary") {
     buttonClass =
-      "text-sky-400 bg-transparent hover:bg-sky-400 hover:text-white font-bold py-2 px-4 rounded-md border border-sky-400 border-width-1";
+      "text-sky-400 bg-transparent hover:bg-sky-400 hover:text-white border border-sky-400 border-width-1";
   } else if (variant === "danger") {
-    buttonClass =
-      "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-md";
+    buttonClass = `bg-red-500 hover:bg-red-700 text-white`;
   } else if (variant === "text") {
-    buttonClass =
-      "text-blue-500 hover:text-sky-400 font-bold py-2 px-4 rounded-md";
+    buttonClass = `text-blue-500 hover:text-sky-400`;
   }
 
-  return <button className={`${buttonClass} ${className}`}>{children}</button>;
+  return (
+    <button className={`${defaultClasses} ${buttonClass} ${className}`}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- app/javascript/components/Content.jsx
+++ app/javascript/components/Content.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Button from "./Button";
 import Table from "./Table";
+import { BarsArrowDownIcon, PlusIcon } from "@heroicons/react/16/solid";
 const sampleData = {
   name: "Deal a Day",
   routes: [
@@ -139,28 +140,34 @@ const Content = () => {
     <div className="pt-16 pl-16 pr-6 bg-zinc-100 min-h-screen">
       <h1 className="text-2xl font-semibold">{name}</h1>
       <p className="my-2">{routes.length} content routes</p>
-      <div className="flex justify-between">
-        <div>
+      <div className="flex justify-between flex-wrap">
+        <div className="flex items-start mb-2 flex-wrap">
           <Button variant="secondary" className="mr-2">
+            <BarsArrowDownIcon className="size-6" />
             Grouped by vSG
           </Button>
           <Button variant="secondary" className="mr-2">
+            <BarsArrowDownIcon className="size-6" />
             Grouped by app
           </Button>
           <Button variant="secondary" className="mr-2">
+            <BarsArrowDownIcon className="size-6" />
             Grouped by Tier
           </Button>
           <Button variant="primary" className="mr-2">
+            <BarsArrowDownIcon className="size-6" />
             Ungrouped
           </Button>
         </div>
-        <div>
+        <div className="flex mb-2">
           <input
             type="text"
             className="bg-transparent border mx-2 h-10 px-2"
             placeholder="Content route search"
           />
-          <Button variant="primary">Add new app</Button>
+          <Button variant="primary">
+            <PlusIcon className="size-6" /> Add new app
+          </Button>
         </div>
       </div>
 
